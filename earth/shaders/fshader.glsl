06d297c805c8e63dc70574febecba9c4c12700e3
mattgraf1 <mgraf1@tulane.edu>
2024-11-22 15:06:00 -0600

    Attempted Day/Night
    Couldnt figure out day/night, lots of patches of light ):

--- earth/shaders/fshader.glsl
+++ earth/shaders/fshader.glsl
@@ -21,9 +21,12 @@ out vec4 fragColor;
 
 void main()
 {
-  vec4 L = normalize( (ModelViewLight*LightPosition) - pos );
+  vec3 L = normalize( (ModelViewLight*LightPosition).xyz - pos.xyz );
+  
   float Kd = 1.0;
   
+    
+    
   vec4 diffuse_color = texture(textureEarth, texCoord );
   diffuse_color = Kd*diffuse_color;
   
@@ -31,7 +34,9 @@ void main()
   vec4 cloudBlend = diffuse_color + cloud_color * 0.5;
   cloudBlend = clamp(cloudBlend, 0.0, 1.0);
     
-  fragColor = ambient + cloudBlend;
+  float diffuse_intensity = max(dot(N.xyz, L), 0.0);
+  vec4 diffuselight = diffuse_intensity * cloudBlend;
+  fragColor = ambient + cloudBlend + diffuselight;
   fragColor = clamp(fragColor, 0.0, 1.0);
   fragColor.a = 1.0;
 }
